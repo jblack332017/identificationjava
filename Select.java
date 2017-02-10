4884084f88641c78857f3950b8f0bf22a3fe0d80
Josh Black <joshblack@tigger.local>
2017-02-10 14:28:44 -0700

    test

--- Select.java
+++ Select.java
@@ -74,7 +74,7 @@ public class Select {
 		}
 		
 		String cmd = "python runBlast.py "+number;
-		File file = new File("/path/to/directory");
+		File file = new File("");
 		String[] directories = file.list(new FilenameFilter() {
 		  @Override
 		  public boolean accept(File current, String name) {
