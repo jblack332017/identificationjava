b09e697417e0f1d93e7aeb2a2281d2cbdf31f010
Josh Black <joshblack@Izzies-iPhone-010010103180.app.byu.edu>
2017-02-15 14:33:29 -0700

    posive == length

--- Select.java
+++ Select.java
@@ -66,7 +66,7 @@ public class Select {
 				    writer.close();
 				    if (number+1 == organisms.size())
 					{
-						System.out.println(bottomCounter+" "+topCounter);
+						System.out.println(number+1+" "+organisms.size());
 					}
 				    
 				    
