d2d0f1d0f1246b3c06e4e6c05f233fd068b237e4
Josh Black <joshblack@byu628750wks.app.byu.edu>
2017-03-06 13:41:37 -0700

    testing the matches

--- Select.java
+++ Select.java
@@ -195,6 +195,7 @@ public class Select {
 	
 	public void removeDuplicates()
 	{
+		System.out.println(matches.lastKey().length());
 		for (String id: matches.keySet()){
 			if (!matches.get(id).inOtherOrg())
 			{
