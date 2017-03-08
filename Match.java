54e47e5e1ce13eee05f2692d1ceb012a61889801
Josh Black <joshblack@tigger.local>
2017-03-08 13:51:01 -0700

    print matches

--- Match.java
+++ Match.java
@@ -114,7 +114,7 @@ public class Match {
 			writer.print(stringBuilder.toString());
 			writer.close();
 			
-			String cmd = "mafft --globalpair --maxiterate 1000 matches/" + id + " > macthes/Out"+id;
+			String cmd = "mafft --globalpair --maxiterate 1000 matches/" + id + " > matches/Out"+id;
 			System.out.println(cmd);
 			Process p = Runtime.getRuntime().exec(cmd);
 			p.waitFor();
