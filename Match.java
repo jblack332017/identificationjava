4229567cf08e99b25b80778241d6dde597e65ef7
Josh Black <joshblack@tigger.local>
2017-03-08 14:02:39 -0700

    print matches

--- Match.java
+++ Match.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -117,6 +118,27 @@ public class Match {
 			String cmd = "/fslhome/jblack33/bin/mafft --globalpair --maxiterate 1000 matches/" + id + " > matches/Out"+id;
 			System.out.println(cmd);
 			Process p = Runtime.getRuntime().exec(cmd);
+			
+			BufferedReader stdInput = new BufferedReader(new 
+				     InputStreamReader(p.getInputStream()));
+
+				BufferedReader stdError = new BufferedReader(new 
+				     InputStreamReader(p.getErrorStream()));
+
+				// read the output from the command
+				System.out.println("Here is the standard output of the command:\n");
+				String s = null;
+				while ((s = stdInput.readLine()) != null) {
+				    System.out.println(s);
+				}
+
+				// read any errors from the attempted command
+				System.out.println("Here is the standard error of the command (if any):\n");
+				while ((s = stdError.readLine()) != null) {
+				    System.out.println(s);
+				}
+			
+			
 			p.waitFor();
 			
 			
