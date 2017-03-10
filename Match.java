b60b3047cd7b8aa3ac0cdeca159304dc79684fa1
Josh Black <joshblack@tigger.local>
2017-03-10 14:24:21 -0700

    test mafft command

--- Match.java
+++ Match.java
@@ -114,7 +114,7 @@ public class Match {
 			
 			writer.print(stringBuilder.toString());
 			writer.close();
-			String[] cmd = {"/fslhome/jblack33/bin/mafft --globalpair --maxiterate 1000 matches/" + id," > matches/Out"+id};
+			String[] cmd = {"/fslhome/jblack33/bin/mafft", "--globalpair --maxiterate 1000 matches/" + id," > matches/Out"+id};
 
 	//String cmd = "/fslhome/jblack33/bin/mafft --globalpair --maxiterate 1000 matches/" + id;// + " > matches/Out"+id;
 			System.out.println(cmd);
